8fee16c261e4cb76c5b32f842e17e9566e79ed1f
Danny Toorop <danny.toorop@hva.nl>
2022-10-12 14:33:47 +0200

    Project setup

---
+++ src/main/java/Word.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class Word implements WordFrequency {
+    private String word;
+    private int frequency;
+
+    @Override
+    public String getWord() {
+        return null;
+    }
+
+    @Override
+    public int getFrequency() {
+        return 0;
+    }
+}
